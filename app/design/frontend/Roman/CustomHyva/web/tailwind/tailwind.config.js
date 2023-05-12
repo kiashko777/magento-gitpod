6b4531a089277820c524e16b7ee83c755ed99d13
Roman Kiashko <kiashko777@gmail.com>
2023-05-12 08:01:11 +0000

    new Hyva theme create
    new Hyva theme created and configured

--- app/design/frontend/Roman/CustomHyva/web/tailwind/tailwind.config.js
+++ app/design/frontend/Roman/CustomHyva/web/tailwind/tailwind.config.js
@@ -125,7 +125,7 @@ module.exports = hyvaModules.mergeTailwindConfig({
     '../../**/*.phtml',
     '../../*/layout/*.xml',
     // parent theme in Vendor (if this is a child-theme)
-    //'../../../../../../../vendor/hyva-themes/magento2-default-theme/**/*.phtml',
+    '../../../../../../../vendor/hyva-themes/magento2-default-theme/**/*.phtml',
     // app/code phtml files (if need tailwind classes from app/code modules)
     //'../../../../../../../app/code/**/*.phtml',
   ]
